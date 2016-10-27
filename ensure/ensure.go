ecb48bd316e39a25a7924b0fea57301cff1af3a4
Alexander Andryashin <aandryashin@GDSpb0615-lan.lupus.griddynamics.net>
2016-10-27 17:32:43 +0300

    Adapted default configuration to latest containers

--- ensure/ensure.go
+++ ensure/ensure.go
@@ -18,12 +18,10 @@ func CloseNotifier(next http.HandlerFunc) http.HandlerFunc {
 
 func Post(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("ensure post")
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
-		log.Println("ensured")
 		next.ServeHTTP(w, r)
 	}
 }
