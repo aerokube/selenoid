07de2064f4e70787fd152e0990560b59494498b4
Alexander Andryashin <aandryashin@gmail.com>
2016-11-08 01:09:10 +0300

    Removed Dump handler because it does not implement CloseNotifier interface.

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"io"
 	"log"
 	"net"
 	"net/http"
-	"net/http/httptest"
-	"net/http/httputil"
 	"net/url"
 	"os"
 	"os/signal"
@@ -45,7 +42,6 @@ func init() {
 	flag.StringVar(&dockerApi, "docker-api", "unix:///var/run/docker.sock", "Docker api url")
 	flag.IntVar(&limit, "limit", 5, "Simultanious container runs")
 	flag.DurationVar(&timeout, "timeout", 60*time.Second, "Session idle timeout in time.Duration format")
-	flag.BoolVar(&logHTTP, "log-http", false, "Log HTTP traffic")
 	flag.Parse()
 	queue = protect.New(limit)
 	var err error
@@ -108,24 +104,7 @@ func Handler() http.Handler {
 	return root
 }
 
-func Dumper(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rdmp, _ := httputil.DumpRequest(r, true)
-		log.Println(string(rdmp))
-		wr := httptest.NewRecorder()
-		h.ServeHTTP(wr, r)
-		wdmp, _ := httputil.DumpResponse(wr.Result(), true)
-		log.Println(string(wdmp))
-		w.WriteHeader(wr.Result().StatusCode)
-		io.Copy(w, wr.Result().Body)
-	})
-}
-
 func main() {
-	h := Handler()
-	if logHTTP {
-		h = Dumper(h)
-	}
 	log.Printf("Listening on %s\n", listen)
-	log.Fatal(http.ListenAndServe(listen, h))
+	log.Fatal(http.ListenAndServe(listen, Handler()))
 }
