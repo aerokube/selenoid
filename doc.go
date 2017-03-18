3678576272aeeab108852cebd474e7d6ca65b24f
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-03-18 19:52:10 +0300

    Added how to send metrics to Graphite with Telegraf

--- doc.go
+++ doc.go
@@ -59,6 +59,65 @@ The following flags are supported:
   -timeout duration
     	Session idle timeout in time.Duration format (default 1m0s)
 
+Status
+
+Selenoid calculates usage statistics that can be accessed with HTTP request:
+  $ curl http://localhost:4444/status
+  {
+    "total": 80,
+    "used": 0,
+    "queued": 0,
+    "pending": 1,
+    "browsers": {
+      "firefox": {
+        "46.0": {
+          "user1": 5,
+          "user2": 6
+        },
+        "48.0": {
+          "user2": 3
+        }
+      }
+    }
+  }
+Users are extracted from basic HTTP authentication headers.
+
+Sending status to external systems
+
+To send Selenoid status data described above you can use Telegraf (https://github.com/influxdata/telegraf). The following steps show how to send status to Graphite:
+1) Pull latest Telegraf Docker container:
+  # docker pull telegraf:alpine
+2) Generate configuration file:
+  # mkdir -p /etc/telegraf
+  # docker run --rm telegraf:alpine --input-filter httpjson --output-filter graphite config > /etc/telegraf/telegraf.conf
+3) Edit file like the following (three dots mean some not shown lines):
+    ...
+  [agent]
+  interval = "10s" # <- adjust this if needed
+    ...
+  [[outputs.graphite]]
+    ...
+  servers = ["my-graphite-host.example.com:2024"] # <- adjust host and port
+    ...
+  prefix = "one_min" # <- something before hostname, can be blank
+    ...
+  template = "host.measurement.field"
+    ...
+  [[inputs.httpjson]]
+    ...
+  name = "selenoid"
+    ...
+  servers = [
+    "http://localhost:4444/status" # <- this is localhost only if Telegraf is run on Selenoid host
+  ]
+4) Start Telegraf container:
+  # docker run --net host -d --name telegraf -v /etc/telegraf:/etc telegraf:alpine --config /etc/telegraf.conf
+Metrics will be sent to my-graphite-host.example.com:2024. Metric names will be like the following:
+  one_min.selenoid_example_com.httpjson_selenoid.pending
+  one_min.selenoid_example_com.httpjson_selenoid.queued
+  one_min.selenoid_example_com.httpjson_selenoid.total
+  ...
+
 Building
 
 See https://github.com/aandryashin/selenoid.
