394e41a82cd28f1a899b33de812b5fa61dabdcfc
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-06-09 06:53:51 +0300

    Copying images for docs

--- travis/docs.sh
+++ travis/docs.sh
@@ -23,8 +23,8 @@ echo "Removing existing files"
 mkdir -p ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}
 rm -rf ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}/*
 
-#echo "Copying images"
-#cp -R ${TRAVIS_BUILD_DIR}/docs/img ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}/img
+echo "Copying images"
+cp -R ${TRAVIS_BUILD_DIR}/docs/img ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}/img
 echo "Generating docs"
 docker run -v ${TRAVIS_BUILD_DIR}/docs/:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAGNAME} -D /documents/output/${TAGNAME} index.adoc
 
