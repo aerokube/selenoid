97ad54274060591958377c860271e68419f4524d
dkostichev <dkostichev@alfabank.ru>
2018-03-06 16:25:24 +0300

    upd struct

--- mesos/scheduler/messages.go
+++ mesos/scheduler/messages.go
@@ -5,15 +5,17 @@ type ID struct {
 	Value string `json:"value"`
 }
 
+type Docker struct {
+	Image        string         `json:"image"`
+	Network      string         `json:"network"`
+	Privileged   bool           `json:"privileged"`
+	PortMappings []PortMappings `json:"port_mappings"`
+}
+
 //Структура для хранения данных о контейнере
 type Container struct {
-	Type string `json:"type"`
-	Docker struct {
-		Image        string         `json:"image"`
-		Network      string         `json:"network"`
-		Privileged   bool           `json:"privileged"`
-		PortMappings []PortMappings `json:"port_mappings"`
-	} `json:"docker"`
+	Type   string `json:"type"`
+	Docker Docker `json:"docker"`
 }
 
 type PortMappings struct {
@@ -26,7 +28,7 @@ type PortMappings struct {
 //Резервируемые ресурсы
 type Resource struct {
 	Name   string  `json:"name"`
-	Ranges *Ranges  `json:"ranges,omitempty"`
+	Ranges *Ranges `json:"ranges,omitempty"`
 	Role   string  `json:"role,omitempty"`
 	Type   string  `json:"type"`
 	Scalar *Scalar `json:"scalar,omitempty"`
@@ -49,49 +51,60 @@ type Range struct {
 type Launch struct {
 	TaskInfos []TaskInfo `json:"task_infos"`
 }
+
+type Command struct {
+	Shell bool `json:"shell"`
+}
+
 type TaskInfo struct {
-	Name    string `json:"name"`
-	TaskID  ID     `json:"task_id"`
-	AgentID ID     `json:"agent_id"`
-	Command struct {
-		Shell bool `json:"shell"`
-	} `json:"command"`
-	Container Container  `json:"container"`
+	Name      string     `json:"name"`
+	TaskID    ID         `json:"task_id"`
+	AgentID   ID         `json:"agent_id"`
+	Command   Command    `json:"command"`
+	Container *Container `json:"container"`
 	Resources []Resource `json:"resources"`
 }
 
+type FrameworkInfo struct {
+	User  string   `json:"user"`
+	Name  string   `json:"name"`
+	Roles []string `json:"roles"`
+}
+
+type Subscribe struct {
+	FrameworkInfo FrameworkInfo `json:"framework_info"`
+}
+
 type SubscribeMessage struct {
-	Type string `json:"type"`
-	Subscribe struct {
-		FrameworkInfo struct {
-			User  string   `json:"user"`
-			Name  string   `json:"name"`
-			Roles []string `json:"roles"`
-		} `json:"framework_info"`
-	} `json:"subscribe"`
+	Type      string    `json:"type"`
+	Subscribe Subscribe `json:"subscribe"`
+}
+
+type Decline struct {
+	OfferIds []ID    `json:"offer_ids"`
+	Filters  Filters `json:"filters"`
 }
 
 type DeclineMessage struct {
-	FrameworkID ID     `json:"framework_id"`
-	Type        string `json:"type"`
-	Decline struct {
-		OfferIds []ID `json:"offer_ids"`
-		Filters struct {
-			RefuseSeconds float64 `json:"refuse_seconds"`
-		} `json:"filters"`
-	} `json:"decline"`
+	FrameworkID ID      `json:"framework_id"`
+	Type        string  `json:"type"`
+	Decline     Decline `json:"decline"`
+}
+
+type Filters struct {
+	RefuseSeconds float64 `json:"refuse_seconds"`
+}
+
+type Accept struct {
+	OfferIds   []ID        `json:"offer_ids"`
+	Operations []Operation `json:"operations"`
+	Filters    Filters     `json:"filters"`
 }
 
 type AcceptMessage struct {
 	FrameworkID ID     `json:"framework_id"`
 	Type        string `json:"type"`
-	Accept struct {
-		OfferIds []ID `json:"offer_ids"`
-		Operations []Operation `json:"operations"`
-		Filters struct {
-			RefuseSeconds float64 `json:"refuse_seconds"`
-		} `json:"filters"`
-	} `json:"accept"`
+	Accept      Accept `json:"accept"`
 }
 
 type Operation struct {
@@ -100,109 +113,152 @@ type Operation struct {
 }
 
 type AcknowledgeMessage struct {
-	FrameworkID ID     `json:"framework_id"`
-	Type        string `json:"type"`
-	Acknowledge struct {
-		AgentID ID     `json:"agent_id"`
-		TaskID  ID     `json:"task_id"`
-		UUID    string `json:"uuid"`
-	} `json:"acknowledge"`
+	FrameworkID ID          `json:"framework_id"`
+	Type        string      `json:"type"`
+	Acknowledge Acknowledge `json:"acknowledge"`
+}
+
+type Acknowledge struct {
+	AgentID ID     `json:"agent_id"`
+	TaskID  ID     `json:"task_id"`
+	UUID    string `json:"uuid"`
+}
+
+type Kill struct {
+	TaskID ID `json:"task_id"`
 }
 
 type KillMessage struct {
 	FrameworkID ID     `json:"framework_id"`
 	Type        string `json:"type"`
-	Kill struct {
-		TaskID ID `json:"task_id"`
-	} `json:"kill"`
+	Kill        Kill   `json:"kill"`
 }
 
-func GetAcceptMessage(frameworkId ID, offers []ID, agentId ID) (AcceptMessage) {
-
+func GetPortMappings() ([]PortMappings) {
 	var portMappings = PortMappings{ContainerPort: 4444}
 	portMappings.Name = "http"
 	portMappings.ContainerPort = 4444
 	portMappings.HostPort = 31005
 	portMappings.Protocol = "tcp"
+	return []PortMappings{portMappings}
+}
 
-	var container = Container{Type: "DOCKER"}
-	container.Docker.Image = "selenoid/chrome"
-	container.Docker.Network = "BRIDGE"
-	container.Docker.Privileged = true
-	container.Docker.PortMappings = append(container.Docker.PortMappings, portMappings)
+func NewContainer() *Container {
+	return &Container{
+		Type: "DOCKER",
+		Docker: Docker{
+			Image:        "selenoid/chrome",
+			Network:      "BRIDGE",
+			Privileged:   true,
+			PortMappings: GetPortMappings(),
+		},
+	}
+}
 
-	var rangePort = Range{}
-	rangePort.Begin = 31005
-	rangePort.End = 31006
+func NewResourcePorts() Resource {
+	var rangePort = Range{
+		Begin: 31005,
+		End:   31006,
+	}
 
-	var ranges = Ranges{[]Range{rangePort}}
+	return Resource{
+		Type: "RANGES",
+		Name: "ports",
+		Ranges: &Ranges{
+			[]Range{rangePort},
+		},
+		Role: "*",
+	}
+}
 
-	var resourcesPorts = Resource{Type: "RANGES"}
-	resourcesPorts.Name = "ports"
-	resourcesPorts.Ranges = &ranges
-	resourcesPorts.Role = "*"
+func NewResourcesContainer(name string, value float64) Resource {
+	return Resource{
+		Type:   "SCALAR",
+		Name:   name,
+		Scalar: &Scalar{value},
+	}
+}
 
-	var resourcesCpu = Resource{Type: "SCALAR"}
-	resourcesCpu.Name = "cpus"
-	resourcesCpu.Scalar = &Scalar{1.0}
+func NewLaunchTaskInfo(agentId ID) (Launch) {
+	var taskInfo = TaskInfo{
+		Name:      "My Task",
+		TaskID:    ID{"12220-3440-12532-my-task"},
+		AgentID:   agentId,
+		Command:   Command{false},
+		Container: NewContainer(),
+		Resources: []Resource{
+			NewResourcePorts(),
+			NewResourcesContainer("cpus", 1.0),
+			NewResourcesContainer("mem", 128.0),
+		},
+	}
 
-	var resourcesMem = Resource{Type: "SCALAR"}
-	resourcesMem.Name = "mem"
-	resourcesMem.Scalar = &Scalar{128.0}
+	return Launch{TaskInfos: []TaskInfo{taskInfo}}
+}
 
-	var taskInfo = TaskInfo{}
-	taskInfo.Name = "My Task"
-	taskInfo.TaskID.Value = "12220-3440-12532-my-task"
-	taskInfo.AgentID = agentId
-	taskInfo.Command.Shell = false
-	taskInfo.Container = container
-	taskInfo.Resources = append(taskInfo.Resources, resourcesPorts, resourcesCpu, resourcesMem)
+func NewOperations(agentId ID) (Operation) {
+	return Operation{
+		Type:   "LAUNCH",
+		Launch: NewLaunchTaskInfo(agentId),
+	}
+}
 
-	var launch = Launch{}
-	launch.TaskInfos = append(launch.TaskInfos, taskInfo)
-
-	var operations = Operation{}
-	operations.Type = "LAUNCH"
-	operations.Launch = launch
-
-	var message = AcceptMessage{FrameworkID: frameworkId,
-		Type: "ACCEPT"}
-	message.Accept.OfferIds = offers
-	message.Accept.Operations = append(message.Accept.Operations, operations)
-	message.Accept.Filters.RefuseSeconds = float64(5.0)
-
-	return message
+func GetAcceptMessage(frameworkId ID, offers []ID, agentId ID) (AcceptMessage) {
+	return AcceptMessage{
+		FrameworkID: frameworkId,
+		Type:        "ACCEPT",
+		Accept: Accept{
+			offers,
+			[]Operation{NewOperations(agentId)},
+			Filters{RefuseSeconds: 5.0},
+		},
+	}
 }
 
 func GetSubscribedMessage(user string, name string, roles []string) (SubscribeMessage) {
-	var message = SubscribeMessage{Type: "SUBSCRIBE"}
-	message.Subscribe.FrameworkInfo.User = user
-	message.Subscribe.FrameworkInfo.Name = name
-	message.Subscribe.FrameworkInfo.Roles = roles
-	return message
+	return SubscribeMessage{
+		Type: "SUBSCRIBE",
+		Subscribe: Subscribe{
+			FrameworkInfo{
+				User:  user,
+				Name:  name,
+				Roles: roles,
+			},
+		},
+	}
 }
 
 func GetAcknowledgeMessage(frameworkId ID, agentId ID, UUID string) (AcknowledgeMessage) {
-	var message = AcknowledgeMessage{FrameworkID: frameworkId,
-		Type: "ACKNOWLEDGE"}
-	message.Acknowledge.AgentID = agentId
-	message.Acknowledge.TaskID = ID{"12220-3440-12532-my-task"}
-	message.Acknowledge.UUID = UUID
-	return message
+	return AcknowledgeMessage{
+		FrameworkID: frameworkId,
+		Type:        "ACKNOWLEDGE",
+		Acknowledge: Acknowledge{
+			AgentID: agentId,
+			TaskID:  ID{"12220-3440-12532-my-task"},
+			UUID:    UUID,
+		},
+	}
 }
 
 func GetDeclineMessage(frameworkId ID, offerId []ID) (DeclineMessage) {
-	var message = DeclineMessage{FrameworkID: frameworkId,
-		Type: "DECLINE"}
-	message.Decline.OfferIds = offerId
-	message.Decline.Filters.RefuseSeconds = 5.0
-	return message
+	return DeclineMessage{
+		FrameworkID: frameworkId,
+		Type:        "DECLINE",
+		Decline: Decline{
+			OfferIds: offerId,
+			Filters: Filters{
+				RefuseSeconds: 5.0,
+			},
+		},
+	}
 }
 
 func GetKillMessage(frameworkId ID) (KillMessage) {
-	var message = KillMessage{
+	return KillMessage{
 		FrameworkID: frameworkId,
-		Type:        "KILL"}
-	message.Kill.TaskID = ID{"12220-3440-12532-my-task"}
-	return message
+		Type:        "KILL",
+		Kill: Kill{
+			TaskID: ID{"12220-3440-12532-my-task"},
+		},
+	}
 }
