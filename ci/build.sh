1ac112a1de2edb8a0734f59d5247ecad3a02b973
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-04-29 16:46:30 +0300

    Migrated to Github Actions (related to #903)

---
+++ ci/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export GO111MODULE="on"
+go get -u github.com/mitchellh/gox # cross compile
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
+gox -os "linux darwin windows" -arch "amd64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
