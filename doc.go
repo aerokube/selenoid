d781a9d0398f517f4d8c347d94d21211c3a176e9
Ivan Krutov <vania-pooh@aerokube.com>
2022-04-19 19:33:37 +0300

    Fixed old-school documentation URL

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-Selenoid is a powerful implementation of Selenium Hub using Docker or standalone web driver binaries to start and launch browsers. Documentation has moved to: http://github.com/aandryashin/selenoid
+Selenoid is a powerful implementation of Selenium Hub using Docker or standalone web driver binaries to start and launch browsers. Documentation has moved to: https://aerokube.com/selenoid/latest.
 */
 package main
