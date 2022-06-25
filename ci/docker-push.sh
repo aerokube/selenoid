3c49924096cccc1c28abd7fb53e2f3530bccecc5
Ivan Krutov <vania-pooh@aerokube.com>
2022-06-25 17:24:47 +0300

    Automatically building image for linux/arm64 (related to #1107)

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -2,10 +2,5 @@
 
 set -e
 
-docker build -t $GITHUB_REPOSITORY .
-docker tag $GITHUB_REPOSITORY $GITHUB_REPOSITORY:$1
-docker tag $GITHUB_REPOSITORY selenoid/hub:$1
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $GITHUB_REPOSITORY
-docker push $GITHUB_REPOSITORY:$1
-docker push selenoid/hub:$1
+docker buildx build --push -t "$GITHUB_REPOSITORY" -t "$GITHUB_REPOSITORY:$1" -t "selenoid/hub:$1" --platform linux/amd64,linux/arm64 .
